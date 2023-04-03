b33ca26cab23eb98f9e89d85055098623c09060e
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-03 12:22:24 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(str = "roller-skate"){
+    return `This Saturday, I want to ${str}!`
+}
+
+const mondayWork = function(str = "go to the office"){
+    return `This Monday, I will ${str}.`
+}
+
+function wrapAdjective(flare = "*") {
+
+    return function(str = "special"){
+        return `You are ${flare}${str}${flare}!`
+    }
+}
